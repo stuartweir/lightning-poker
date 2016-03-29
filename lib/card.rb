3f78d9edef2dff79e1641adf17edd1b52d28fd01
stuartweir <cameron.weir@mytwu.ca>
2016-03-28 22:04:14 -0700

    second commit

--- lib/card.rb
+++ lib/card.rb
@@ -3,7 +3,7 @@ class Card
     new(suit: suit, rank: rank)
   end
   private_class_method :new
-  
+
   def initialize(suit:, rank:)
     @suit = suit
     @rank = case rank
@@ -13,25 +13,45 @@ class Card
       else rank
     end
   end
-  
+
   def suit
     @suit
   end
-  
+
   def rank
     @rank
   end
-  
+
   def ==(other)
     rank == other.rank && suit == other.suit
   end
-  
+
   def hash
     [rank, suit].hash
   end
-  
+
   def eql?(other)
     self == other
   end
-end 
 
+  def to_s
+    id = if rank > 10
+      {
+        11 => "J",
+        12 => "Q",
+        13 => "K"
+      }.fetch(rank)
+    else
+      rank.to_s
+    end
+
+    s = {
+      hearts: "♡ ",
+      spades: "♤ ",
+      diamonds: "♢ ",
+      clubs: "♧ ",
+    }
+
+    "#{id.upcase}#{s.fetch(suit)}"
+  end
+end
--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -1,39 +1,38 @@
-require 'tmpdir'
 require 'pty'
 
 BIN = File.expand_path("../../bin/play", __FILE__)
 
-describe 'CLI' do
-  
-  before(:example) do
+describe 'CLI', :acceptance do
+  def run_app(&block)
     dir = Dir.tmpdir + '/highcard_test_state'
-    'rm -Rf #{dir}'
-    'mkdir -p #{dir}'
+    `rm -Rf #{dir}`
+    `mkdir -p #{dir}`
     ENV['HIGHCARD_DIR'] = dir
+    PTY.spawn(BIN, &block)
   end
-  
+
   example 'it works' do
-    PTY.spawn(BIN) do |output, input, pid|
+    run_app do |output, input, pid|
       sleep 0.5
-      
+
       input.write("y\n")
-      
+
       sleep 0.5
-      
+
       buffer = output.read_nonblock(1024)
       puts buffer
       raise unless buffer.include?("You won") || buffer.include?("You lost")
     end
   end
-  
-  example 'betting on a winning hand' do
-    PTY.spawn(BIN) do |output, input, pid|
+
+  example 'betting on winning hand' do
+    run_app do |output, input, pid|
       sleep 0.5
-      
+
       input.write("y\n")
-      
+
       sleep 0.5
-      
+
       buffer = output.read_nonblock(1024)
       puts buffer
       raise unless buffer.include?("You won") || buffer.include?("You lost")
